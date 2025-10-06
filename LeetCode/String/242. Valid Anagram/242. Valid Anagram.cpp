32c2e644646373c4ed2077dee311a4babbe75e9f
yashvi-3106 <yashvidholakiya.cg@gmail.com>
2025-10-06 11:40:57 +0530

    TIME: 8 ms (8.09%) | MEMORY: 9.70 MB (48.65%) .cpp

---
+++ LeetCode/String/242. Valid Anagram/242. Valid Anagram.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) return false;
+        map<char,int> freq;
+        for(int i = 0; i< s.length() ; i++){
+            freq[s[i]]++;
+        }
+        for(int  i = 0; i < t.size() ; i++){
+            freq[t[i]]--;
+            if(freq[t[i]] < 0 ){
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+
+
+// class Solution {
+// public:
+//     bool isAnagram(string s, string t) {
+//         if (s.length() != t.length()) return false;
+//         map<char,int> freq;
+//         for(int i = 0; i< s.length() ; i++){
+//             freq[s[i]]++;
+//         }
+//         map<char,int>freq1 ;
+//         for(int  i = 0; i < t.size() ; i++){
+//             freq1[t[i]]++;
+//         }
+//         return freq == freq1;
+//     }
+// };
+
+
+
+// class Solution {
+// public:
+//     bool isAnagram(string s, string t) {
+//         if (s.length() != t.length()) return false;
+//         vector<int> count(26, 0);
+//         for (int i = 0; i < s.length(); i++) {
+//             count[s[i] - 'a']++;
+//             count[t[i] - 'a']--;
+//         }
+
+//         for (int i = 0; i < 26; i++) {
+//             if (count[i] != 0) return false;
+//         }
+
+//         return true;
+//     }
+// };
