515433d5aa6623102b6da19f311c98ba2b99fa46
yashvi-3106 <yashvidholakiya.cg@gmail.com>
2025-10-15 10:33:44 +0530

    TIME: 0 ms (100.00%) | MEMORY: 8.76 MB (83.13%) .cpp

---
+++ LeetCode/Stack/20. Valid Parentheses/20. Valid Parentheses.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(int i = 0; i < s.size(); i++) {
+            if(s[i] == '(' || s[i] == '[' || s[i] == '{') {
+                st.push(s[i]);
+            } else {
+                if(st.empty()) return false;
+                char top = st.top();
+                st.pop();
+                if(s[i] == ')' && top != '(') return false;
+                if(s[i] == ']' && top != '[') return false;
+                if(s[i] == '}' && top != '{') return false;
+            }
+        }
+        return st.empty();
+    }
+};
