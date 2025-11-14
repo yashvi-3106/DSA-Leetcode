f9af3b10744f16b9257c8a2a6301751fc1b63578
yashvi-3106 <yashvidholakiya.cg@gmail.com>
2025-11-14 14:52:01 +0530

    Time: 2 ms (15.51%) | Memory: 9 MB (20.38%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        unordered_map<char,char> mp = { {')','('}, {'}','{'}, {']','['} };
+
+        for (char c : s) {
+            if (mp.find(c) == mp.end()) {
+                st.push(c);
+            }else { 
+                if (st.empty() || st.top() != mp[c]) {
+                    return false;
+                }
+                st.pop();
+            }
+        }
+
+        return st.empty();
+    }
+};
+
+
+
+// class Solution {
+// public:
+//     bool isValid(string s) {
+//         stack<char> st;
+//         for(int i = 0; i < s.size(); i++) {
+//             if(s[i] == '(' || s[i] == '[' || s[i] == '{') {
+//                 st.push(s[i]);
+//             } else {
+//                 if(st.empty()) return false;
+//                 char top = st.top();
+//                 st.pop();
+//                 if(s[i] == ')' && top != '(') return false;
+//                 if(s[i] == ']' && top != '[') return false;
+//                 if(s[i] == '}' && top != '{') return false;
+//             }
+//         }
+//         return st.empty();
+//     }
+// };
