6ed7b38f130ed9e54e948b7b1ce837d0be1a002d
yashvi-3106 <yashvidholakiya.cg@gmail.com>
2025-11-25 12:11:51 +0530

    Time: 4 ms (52.33%) | Memory: 118 MB (83.46%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -8,21 +8,60 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
 class Solution {
 public:
-    bool isPalindrome(ListNode* head) {
-        vector<int> temp;
-         while (head) {
-            temp.push_back(head->val);
-            head = head->next;
+    ListNode* reverse(ListNode* head){
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+        while (curr != nullptr) {
+            ListNode* next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
         }
-        int i = 0, j = temp.size() - 1;
-        while (i < j) {
-            if (temp[i] != temp[j]) return false;
-            i++;
-            j--;
+        return prev;
+    }
+
+    bool isPalindrome(ListNode* head) {
+        if (!head || !head->next) return true;
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        ListNode* right = reverse(slow);
+        ListNode* left = head;
+        while (right) {
+            if (left->val != right->val)
+                return false;
+            left = left->next;
+            right = right->next;
         }
 
         return true;
     }
 };
+
+
+
+// class Solution {
+// public:
+//     bool isPalindrome(ListNode* head) {
+//         vector<int> temp;
+//          while (head) {
+//             temp.push_back(head->val);
+//             head = head->next;
+//         }
+//         int i = 0, j = temp.size() - 1;
+//         while (i < j) {
+//             if (temp[i] != temp[j]) return false;
+//             i++;
+//             j--;
+//         }
+
+//         return true;
+//     }
+// };
