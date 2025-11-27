64273081d707551fc2d8f55a71a856f9f22347bb
yashvi-3106 <yashvidholakiya.cg@gmail.com>
2025-11-27 14:45:04 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (97.60%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -2,41 +2,18 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char> st;
-        unordered_map<char,char> mp = { {')','('}, {'}','{'}, {']','['} };
-
-        for (char c : s) {
-            if (mp.find(c) == mp.end()) {
-                st.push(c);
-            }else { 
-                if (st.empty() || st.top() != mp[c]) {
-                    return false;
-                }
+        for(int i = 0 ; i < s.size() ; i++){
+            if(s[i] == '(' || s[i] == '{' || s[i] == '['){
+                st.push(s[i]);
+            }else{
+                if(st.empty()) return false;
+                char top = st.top();
                 st.pop();
+                if(s[i] == ')' && top != '(') return false;
+                if(s[i] == '}' && top != '{') return false;
+                if(s[i] == ']' && top != '[') return false;
             }
         }
-
         return st.empty();
     }
 };
-
-
-
-// class Solution {
-// public:
-//     bool isValid(string s) {
-//         stack<char> st;
-//         for(int i = 0; i < s.size(); i++) {
-//             if(s[i] == '(' || s[i] == '[' || s[i] == '{') {
-//                 st.push(s[i]);
-//             } else {
-//                 if(st.empty()) return false;
-//                 char top = st.top();
-//                 st.pop();
-//                 if(s[i] == ')' && top != '(') return false;
-//                 if(s[i] == ']' && top != '[') return false;
-//                 if(s[i] == '}' && top != '{') return false;
-//             }
-//         }
-//         return st.empty();
-//     }
-// };
