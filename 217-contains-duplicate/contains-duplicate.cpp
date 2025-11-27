795932f06314bdbbfeab48a1193ca348fe87eaa8
yashvi-3106 <yashvidholakiya.cg@gmail.com>
2025-11-27 14:37:38 +0530

    Time: 51 ms (56.20%) | Memory: 90.8 MB (44.97%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,72 +1,13 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_set<int> ans;
-        for (int i = 0; i < nums.size(); i++) {
-            if (ans.count(nums[i]) > 0) {
+        unordered_set<int> num;
+        for(int n : nums){
+            if(num.find(n) != num.end()){
                 return true;
-            } 
-            ans.insert(nums[i]);
+            }
+            num.insert(n);
         }
         return false;
     }
 };
-
-// class Solution {
-// public:
-//     bool containsDuplicate(vector<int>& nums) {
-//         unordered_set<int> numSet;
-
-//         for (int n : nums) {
-//             if (numSet.find(n) != numSet.end()) {
-//                 return true;
-//             }
-//             numSet.insert(n);
-//         }
-        
-//         return false;        
-//     }
-// };
-
-
-// class Solution {
-// public:
-//     bool containsDuplicate(vector<int>& nums) {
-//         int n = nums.size();
-//         for (int i = 0; i < n - 1; i++) {
-//             for (int j = i + 1; j < n; j++) {
-//                 if (nums[i] == nums[j])
-//                     return true;
-//             }
-//         }
-//         return false;
-//     }
-// };
-
-// class Solution {
-// public:
-//     bool containsDuplicate(vector<int>& nums) {
-//         sort(nums.begin(), nums.end());
-//         for (int i = 0; i < nums.size() - 1; i++) {
-//             if (nums[i] == nums[i + 1]) {
-//                 return true;
-//             }
-//         }
-//         return false;
-//     }
-// };
-
-
-// class Solution {
-// public:
-//     bool containsDuplicate(vector<int>& nums) {
-//         int n = nums.size();
-//         for (int i = 0; i < n - 1; i++) {
-//             for (int j = i + 1; j < n; j++) {
-//                 if (nums[i] == nums[j])
-//                     return true;
-//             }
-//         }
-//         return false;
-//     }
-// };
