c4cad2a068a0f0950e516581918b7ced5d2b4d3b
yashvi-3106 <yashvidholakiya.cg@gmail.com>
2025-12-26 18:09:26 +0530

    Time: 0 ms (100.00%) | Memory: 22.6 MB (77.70%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -1,13 +1,29 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-    int i=0;
-    for(int j =0 ; j<nums.size();j++){
-        if(nums[i]!=nums[j]){
-            i++;
-            nums[i] = nums[j]; 
+        if(nums.empty()) return 0;
+        int j = 1;
+        for(int i = 1; i< nums.size(); i++){
+            if(nums[i]!=nums[i-1]){
+                nums[j] = nums[i];
+            j++;
+            }
         }
-    }
-    return i+1;
+        return j;
     }
 };
+
+
+// class Solution {
+// public:
+//     int removeDuplicates(vector<int>& nums) {
+//     int i=0;
+//     for(int j =0 ; j<nums.size();j++){
+//         if(nums[i]!=nums[j]){
+//             i++;
+//             nums[i] = nums[j]; 
+//         }
+//     }
+//     return i+1;
+//     }
+// };
