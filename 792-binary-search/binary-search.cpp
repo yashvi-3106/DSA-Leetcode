b0edcdb082348dd3be1b09970d7d33e3911cc4e3
yashvi-3106 <yashvidholakiya.cg@gmail.com>
2026-01-02 14:13:00 +0530

    Time: 0 ms (100.00%) | Memory: 31.3 MB (79.15%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -5,12 +5,12 @@ public:
         int b = nums.size() - 1;
         while(a<=b){
             int c = (a+b)/2;
-            if(nums[c]==target){
+            if(nums[c] == target){
                 return c;
-            }else if(nums[c]<target){
-                a = c+ 1;
+            }else if(nums[c] < target){
+                a = c + 1;
             }else{
-                b = c -1;
+                b = c - 1;
             }
         }
         return -1;
