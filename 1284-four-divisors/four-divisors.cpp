b30108d42e8f32848e4c0617743434297ab1e994
yashvi-3106 <yashvidholakiya.cg@gmail.com>
2026-01-04 23:08:01 +0530

    Time: 6 ms (86.70%) | Memory: 16 MB (92.61%) - LeetSync

--- 1284-four-divisors/four-divisors.cpp
+++ 1284-four-divisors/four-divisors.cpp
@@ -1,26 +1,53 @@
 class Solution {
 public:
     int sumFourDivisors(vector<int>& nums) {
-        int ans = 0;
+        int total = 0;
         for (int num : nums) {
-            int cnt = 0;
-            int sum = 0;
-            for (int div = 1; div * div <= num; div++) {
-                if (num % div == 0) {
-                    int d1 = div;
-                    int d2 = num / div;
-                    if (d1 == d2) {
-                        cnt += 1;
-                        sum += d1;
-                    } else {
-                        cnt += 2;
-                        sum += d1 + d2;
+            int cnt = 0, sum = 0;
+            for (int i = 1; i * i <= num; i++) {
+                if (num % i == 0) {
+                    int otherDivisor = num / i;
+                    cnt++;
+                    sum += i;
+                    if (i != otherDivisor) {
+                        cnt++;
+                        sum += otherDivisor;
                     }
+                    if (cnt > 4) break;
                 }
             }
-            if (cnt == 4)
-                ans += sum;
+            if (cnt == 4) {
+                total += sum;
+            }
         }
-        return ans;
+        return total;
     }
 };
+
+
+// class Solution {
+// public:
+//     int sumFourDivisors(vector<int>& nums) {
+//         int ans = 0;
+//         for (int num : nums) {
+//             int cnt = 0;
+//             int sum = 0;
+//             for (int div = 1; div * div <= num; div++) {
+//                 if (num % div == 0) {
+//                     int d1 = div;
+//                     int d2 = num / div;
+//                     if (d1 == d2) {
+//                         cnt += 1;
+//                         sum += d1;
+//                     } else {
+//                         cnt += 2;
+//                         sum += d1 + d2;
+//                     }
+//                 }
+//             }
+//             if (cnt == 4)
+//                 ans += sum;
+//         }
+//         return ans;
+//     }
+// };
