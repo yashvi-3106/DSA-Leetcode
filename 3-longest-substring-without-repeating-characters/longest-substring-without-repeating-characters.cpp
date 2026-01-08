3013ba2da3d96100d1e7dc0a630980ff2b6af4b3
yashvi-3106 <yashvidholakiya.cg@gmail.com>
2026-01-08 11:05:04 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (89.90%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,16 +1,62 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int maxLen = 0;
-        for (int i = 0; i < s.length(); i++) {
-            int hash[256] = {0};   
-            for (int j = i; j < s.length(); j++) {
-                if (hash[s[j]] == 1) break;
-                hash[s[j]] = 1;
-                int length = j - i + 1;
-                maxLen = max(maxLen, length);
+        int hash[256];
+        for (int i = 0; i < 256; i++) hash[i] = -1;
+        int left = 0, right = 0, maxLen = 0;
+        while (right < s.length()) {
+            if (hash[s[right]] != -1) {
+                if (hash[s[right]] >= left) {
+                    left = hash[s[right]] + 1;
+                }
             }
+            int length = right - left + 1;
+            maxLen = max(maxLen, length);
+            hash[s[right]] = right;
+            right++;
         }
         return maxLen;
     }
 };
+
+// class Solution {
+// public:
+//     int lengthOfLongestSubstring(string s) {
+//         int maxLen = 0;
+//         for (int i = 0; i < s.length(); i++) {
+//             int hash[256] = {0};   
+//             for (int j = i; j < s.length(); j++) {
+//                 if (hash[s[j]] == 1) break;
+//                 hash[s[j]] = 1;
+//                 int length = j - i + 1;
+//                 maxLen = max(maxLen, length);
+//             }
+//         }
+//         return maxLen;
+//     }
+// };
+
+
+// class Solution {
+// public:
+//     int lengthOfLongestSubstring(string s) {
+//         map<char, int> mp;
+//         int maxLen = 0;
+//         int n = s.size();
+//         int l=0, r=0;
+//         while(r<n){
+//             if(mp.find(s[r]) == mp.end()  or mp[s[r]] < l){
+//                 int len = r-l+1;
+//                 maxLen = max(maxLen, len);
+//                 mp[s[r]] = r;                
+//             }
+//             else{
+//                 l = mp[s[r]] + 1;
+//                 mp[s[r]] = r;
+//             }
+//             r++;
+//         }
+//         return maxLen;
+//     }
+// };
+
