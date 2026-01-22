6a42c920f39d9a445be8061e91d5bb597307b6e8
yashvi-3106 <yashvidholakiya.cg@gmail.com>
2026-01-22 10:20:21 +0530

    Time: 0 ms (100.00%) | Memory: 55.2 MB (32.04%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -1,11 +1,34 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        unordered_set<int> st(nums.begin(),nums.end());
-        int i = 1;
-        while(st.count(i)){
+        int i = 0;
+        while (i < nums.size()) {
+            if (nums[i] > 0 && nums[i] <= nums.size()) {
+                int corr = nums[i] - 1;   // SAFE now
+                if (nums[i] != nums[corr]) {
+                    swap(nums[i], nums[corr]);
+                    continue;
+                }
+            }
             i++;
         }
-        return i;
+        for(int i = 0; i < nums.size();i++){
+            if(nums[i] != i + 1){
+                return i + 1;
+            }
+        }
+        return nums.size() + 1;
     }
 };
+
+// class Solution {
+// public:
+//     int firstMissingPositive(vector<int>& nums) {
+//         unordered_set<int> st(nums.begin(),nums.end());
+//         int i = 1;
+//         while(st.count(i)){
+//             i++;
+//         }
+//         return i;
+//     }
+// };
