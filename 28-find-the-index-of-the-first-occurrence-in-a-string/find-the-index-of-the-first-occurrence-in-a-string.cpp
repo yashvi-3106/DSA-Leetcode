fd61229566af499b508d166f8b36cd55e37feba2
yashvi-3106 <yashvidholakiya.cg@gmail.com>
2026-01-22 11:09:33 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (37.77%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -2,13 +2,12 @@ class Solution {
 public:
     int strStr(string haystack, string needle) {
         int nIndex = 0;
-        for(int i = 0; i < haystack.length(); i++ ){
-            if(haystack[i]==needle[nIndex]){
+        for(int i = 0; i < haystack.length(); i++){
+            if(haystack[i] == needle[nIndex]){
                 nIndex++;
-            }
-            else{
-                i=i-nIndex;
-                nIndex=0;
+            }else{
+                i = i - nIndex;
+                nIndex = 0;
             }
             if(nIndex == needle.length()){
                 return i - needle.length() + 1;
@@ -17,3 +16,23 @@ public:
         return -1;
     }
 };
+
+// class Solution {
+// public:
+//     int strStr(string haystack, string needle) {
+//         int nIndex = 0;
+//         for(int i = 0; i < haystack.length(); i++ ){
+//             if(haystack[i]==needle[nIndex]){
+//                 nIndex++;
+//             }
+//             else{
+//                 i=i-nIndex;
+//                 nIndex=0;
+//             }
+//             if(nIndex == needle.length()){
+//                 return i - needle.length() + 1;
+//             }
+//         }
+//         return -1;
+//     }
+// };
