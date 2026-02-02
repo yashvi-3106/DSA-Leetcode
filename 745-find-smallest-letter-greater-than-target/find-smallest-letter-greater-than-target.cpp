af921cdfa066c36e5c0d0d715ba250b6948dbfe8
yashvi-3106 <yashvidholakiya.cg@gmail.com>
2026-02-02 13:36:33 +0530

    Time: 0 ms (100.00%) | Memory: 19.7 MB (86.90%) - LeetSync

--- 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.cpp
+++ 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.cpp
@@ -1,16 +1,28 @@
 class Solution {
 public:
     char nextGreatestLetter(vector<char>& letters, char target) {
-        int low = 0;
-        int high =  letters.size() - 1;
-        while(low <= high){
-            int mid = (low + high) / 2;
-            if(letters[mid] <= target) {
-                low = mid + 1;
-            }else{
-                high = mid - 1;
+        for(int i=0; i<letters.size(); i++){
+            if((int)target < (int)letters[i]){
+                return letters[i];
             }
         }
-        return letters[low % letters.size()];
+        return letters[0];
     }
 };
+
+// class Solution {
+// public:
+//     char nextGreatestLetter(vector<char>& letters, char target) {
+//         int low = 0;
+//         int high =  letters.size() - 1;
+//         while(low <= high){
+//             int mid = (low + high) / 2;
+//             if(letters[mid] <= target) {
+//                 low = mid + 1;
+//             }else{
+//                 high = mid - 1;
+//             }
+//         }
+//         return letters[low % letters.size()];
+//     }
+// };
