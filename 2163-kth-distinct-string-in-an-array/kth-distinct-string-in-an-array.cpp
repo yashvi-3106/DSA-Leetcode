b6cea3ac094e710938e0e1e24627215ff9cb58f1
yashvi-3106 <yashvidholakiya.cg@gmail.com>
2026-02-12 10:13:19 +0530

    Time: 9 ms (34.16%) | Memory: 18.3 MB (51.89%) - LeetSync

---
+++ 2163-kth-distinct-string-in-an-array/kth-distinct-string-in-an-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string kthDistinct(vector<string>& arr, int k) {
+        unordered_map<string,int> freq;
+        for(string s : arr){
+            freq[s]++;
+        }
+        for(string s : arr){
+            if(freq[s] == 1){
+                k--;
+                if(k == 0) return s;
+            }
+        }
+        return "";
+    }
+};
