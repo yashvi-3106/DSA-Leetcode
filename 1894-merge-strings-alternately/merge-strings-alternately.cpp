49030dad9658dafa32afa948f0b1bd536cf9e169
yashvi-3106 <yashvidholakiya.cg@gmail.com>
2026-03-05 10:07:46 +0530

    Time: 4 ms (31.63%) | Memory: 8.5 MB (64.11%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.cpp
+++ 1894-merge-strings-alternately/merge-strings-alternately.cpp
@@ -3,13 +3,9 @@ public:
     string mergeAlternately(string word1, string word2) {
         string result = "";
         int i = 0;
-        while(i < word1.length() || i < word2.length()){
-            if( i < word1.length()){
-                result += word1[i];
-            }
-            if( i < word2.length()){
-                result += word2[i];
-            }
+        while(i < word1.size() || i < word2.size()){
+            if(i < word1.size()) result += word1[i];
+            if(i < word2.size()) result += word2[i];
             i++;
         }
         return result;
