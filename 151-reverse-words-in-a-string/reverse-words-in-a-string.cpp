dfd13983b49620a3696d09158a812c90743e42ef
yashvi-3106 <yashvidholakiya.cg@gmail.com>
2026-03-10 09:26:49 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (69.52%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,56 +1,21 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        int l = 0, r = s.size() - 1;
-        // reverse entire string firzt
-        while(l < r){
-            swap(s[l++],s[r--]);
-        }
-        int i = 0, start = 0, end = 0;
-        // now reverse each word and clean space
-        while(i < s.size()){
-
-            // removing spaces
-            while(i < s.size() && s[i] == ' ') i++;
-            if(i >= s.size()) break;
-            
-            if (end > 0) s[end++] = ' ';
-
+        int i = 0, j = s.size() - 1;
+        while(i<j) swap(s[i++],s[j--]);
+        int k = 0, start = 0, end = 0;
+        while(k < s.size()){
+            while(k < s.size() && s[k] == ' ') k++;
+            if(k >= s.size()) break;
+            if(end > 0) s[end++] = ' ';
             start = end;
-            // copy characters of word
-            while(i < s.size() && s[i] != ' ') s[end++] = s[i++];
-            l = start , r = end - 1;
-            
-            //  reverse current word
-            while(l < r) swap(s[l++],s[r--]);
+            while( k < s.size() && s[k] != ' '){
+                s[end++] = s[k++];
+            }
+            i = start;
+            j = end - 1;
+            while(i < j) swap(s[i++],s[j--]);
         }
         return s.substr(0,end);
     }
 };
-
-// class Solution {
-// public:
-//     string reverseWords(string s) {
-//         int i = 0;
-//         int j = s.size() - 1;
-//         while(i<j){
-//             swap(s[i],s[j]);
-//             i++;
-//             j--;
-//         }
-//         string res = "";
-//         for(int i = 0 ;i < s.size() ; i++){
-//             string word = "";
-//             while(i<s.size() && s[i]!=' '){
-//                 word+=s[i];
-//                 i++;
-//             }
-//             reverse(word.begin(),word.end());
-//             if(word.size() > 0){
-//                 res = res + " " + word;
-//             }
-//         }
-
-//         return res.substr(1);
-//     }
-// };
