0a90d97b88c196f4bb8744f8ddfb5109d64202f0
yashvi-3106 <yashvidholakiya.cg@gmail.com>
2026-04-10 15:39:00 +0530

    Time: 5 ms (26.53%) | Memory: 9.8 MB (23.82%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -2,52 +2,10 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         if(s.length() != t.length()) return false;
-        map<char,int> freq;
-        for(int i = 0; i < s.length(); i++){
-            freq[s[i]]++;
-        }
-        for(int i = 0; i < t.length(); i++){
-            freq[t[i]]--;
-            if(freq[t[i]] < 0) return false;
-        }
-        return true;
+        map<char,int> freq1;
+        map<char,int> freq2;
+        for(char c : s) freq1[c]++;
+        for(char c : t) freq2[c]++;
+        return freq1 == freq2;
     }
 };
-
-
-
-// class Solution {
-// public:
-//     bool isAnagram(string s, string t) {
-//         if (s.length() != t.length()) return false;
-//         map<char,int> freq;
-//         for(int i = 0; i< s.length() ; i++){
-//             freq[s[i]]++;
-//         }
-//         map<char,int>freq1 ;
-//         for(int  i = 0; i < t.size() ; i++){
-//             freq1[t[i]]++;
-//         }
-//         return freq == freq1;
-//     }
-// };
-
-
-
-// class Solution {
-// public:
-//     bool isAnagram(string s, string t) {
-//         if (s.length() != t.length()) return false;
-//         vector<int> count(26, 0);
-//         for (int i = 0; i < s.length(); i++) {
-//             count[s[i] - 'a']++;
-//             count[t[i] - 'a']--;
-//         }
-
-//         for (int i = 0; i < 26; i++) {
-//             if (count[i] != 0) return false;
-//         }
-
-//         return true;
-//     }
-// };
