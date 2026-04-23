6cc302a6b1be444032709fbb16dcd95bdf5ecda4
yashvi-3106 <yashvidholakiya.cg@gmail.com>
2026-04-23 20:44:51 +0530

    Time: 0 ms (100.00%) | Memory: 97.4 MB (24.33%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -10,22 +10,3 @@ public:
         return maxProfit;
     }
 };
-
-
-// class Solution {
-// public:
-//     int maxProfit(vector<int>& prices) {
-//         int minPrice = INT_MAX;
-//         int maxProfit = 0;
-//         for(int price : prices){
-//             if(price < minPrice){
-//                 minPrice = price;
-//             }else if(price - minPrice > maxProfit){
-//                 maxProfit = price - minPrice;
-//             }
-//         }
-//         return maxProfit;
-//     }
-// };
-
-
