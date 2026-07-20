9eb68bf297b012903eebb7be6f80dad55c700705
yashvi-3106 <yashvidholakiya.cg@gmail.com>
2026-07-20 10:33:34 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (85.49%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
+        vector<int> freqP(26), freqS(26);
+        int k = p.size();
         vector<int> res;
         if(s.size() < p.size()) return res;
-        int n = s.size();
-        vector<int> freqS(26), freqP(26);
-        int k = p.size();
-        for(char c : p){
-            freqP[c - 'a']++;
-           
-        }
-        for(int i = 0; i < k; i++){
-            freqS[s[i]-'a']++;
-        }
-        if(freqP== freqS) res.push_back(0);
-        for(int i = k ; i < s.size(); i++){
+        for(int i = 0; i < p.size(); i++) freqP[p[i] - 'a']++;
+        for(int i = 0; i < k; i++) freqS[s[i] - 'a']++;
+        if(freqP == freqS) res.push_back(0);
+        for(int i = k; i < s.size(); i++){
             freqS[s[i] - 'a']++;
-            freqS[s[i - k] - 'a']--;
+            freqS[s[i-k] - 'a']--;
 
-            if(freqS == freqP) res.push_back(i-k+1);
+            if(freqS == freqP) res.push_back(i - k + 1);
         }
         return res;
     }
