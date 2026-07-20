7070226c0923745681534384e40d67ec02322f17
yashvi-3106 <yashvidholakiya.cg@gmail.com>
2026-07-20 10:16:39 +0530

    Time: 104 ms (48.92%) | Memory: 95.2 MB (93.54%) - LeetSync

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.cpp
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -8,14 +8,13 @@ public:
             freq[nums[i]]++;
         }
         if(freq.size() == k) maxSum = sum;
-        for(int i = k ; i < nums.size(); i++){
+        for(int i = k; i < nums.size(); i++){
             sum += nums[i];
             freq[nums[i]]++;
-            sum -= nums[i - k];
-            freq[nums[i - k]]--;
+            sum -= nums[i-k];
+            freq[nums[i-k]]--;
 
-            if(freq[nums[i - k]] == 0) freq.erase(nums[i-k]);
-
+            if(freq[nums[i-k]] == 0) freq.erase(nums[i - k]);
             if(freq.size() == k) maxSum = max(maxSum, sum);
         }
         return maxSum;
