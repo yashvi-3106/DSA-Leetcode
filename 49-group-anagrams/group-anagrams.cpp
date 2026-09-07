ccdc722de96f30dd1960c67d0684cfd6245db762
yashvi-3106 <yashvidholakiya.cg@gmail.com>
2026-09-07 15:04:31 +0530

    Time: 2009 ms (5.02%) | Memory: 26.3 MB (31.90%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,16 +1,45 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string, vector<string>> mp;
-        for(auto ch : strs){
-            string key = ch;
-            sort(key.begin(),key.end());
-            mp[key].push_back(ch);
+
+        int n = strs.size();
+
+        // 2D matrix: n rows, 26 columns
+        vector<vector<int>> freq(n, vector<int>(26, 0));
+
+        for(int i = 0; i < n; i++) {
+
+            for(char ch : strs[i]) {
+                freq[i][ch - 'a']++;
+            }
         }
-        vector<vector<string>> res;
-        for(auto ch : mp){
-            res.push_back(ch.second);
+
+        vector<vector<string>> ans;
+        vector<bool> visited(n, false);
+
+        for(int i = 0; i < n; i++) {
+
+            if(visited[i])
+                continue;
+
+            vector<string> group;
+            group.push_back(strs[i]);
+            visited[i] = true;
+
+            for(int j = i + 1; j < n; j++) {
+
+                if(visited[j])
+                    continue;
+
+                if(freq[i] == freq[j]) {
+                    group.push_back(strs[j]);
+                    visited[j] = true;
+                }
+            }
+
+            ans.push_back(group);
         }
-        return res;
+
+        return ans;
     }
 };
