69f53422b83a01c885036a8973e6ca3672d4700d
yashvi-3106 <yashvidholakiya.cg@gmail.com>
2026-09-25 10:52:37 +0530

    Time: 31 ms (5.35%) | Memory: 22.2 MB (6.09%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,51 +1,23 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        vector<int> col(matrix[0].size(),1);
-        vector<int> row(matrix.size(),1);
-        for(int i = 0; i < matrix.size() ; i++){
-            for(int j = 0; j < matrix[0].size() ; j++){
+        vector<pair<int,int>> res;
+        for(int i = 0; i < matrix.size(); i++){
+            for(int j = 0; j < matrix[0].size(); j++){
                 if(matrix[i][j] == 0){
-                    row[i] = 0;
-                    col[j] = 0;
+                    res.push_back({i, j});
                 }
             }
         }
-        for(int i = 0; i < matrix.size() ; i++){
+        for(int i = 0; i < res.size(); i++){
+            int row = res[i].first;
+            int col = res[i].second;
             for(int j = 0; j < matrix[0].size(); j++){
-                if(row[i] == 0 || col[j] == 0){
-                    matrix[i][j] = 0;
-                }
+                matrix[row][j]= 0;
+            }
+            for(int k = 0; k < matrix.size(); k++){
+                matrix[k][col] = 0;
             }
         }
     }
 };
-
-
-
-// this approach will not work for all matrix
-
-// class Solution {
-// public:
-//     void setZeroes(vector<vector<int>>& matrix) {
-//         for (int i = 0; i < matrix.size(); i++) {
-//             for (int j = 0; j < matrix[0].size(); j++) {
-//                 if (matrix[i][j] == 0) {
-//                     for (int k = 0; k < matrix[0].size(); k++) {
-//                         matrix[i][k] = -1;
-//                     }
-//                     for (int k = 0; k < matrix.size(); k++) {
-//                         matrix[k][j] = -1;
-//                     }
-//                 }
-//             }
-//         }
-//         for(int i = 0; i < matrix.size();i++){
-//             for(int j = 0; j < matrix[0].size();j++){
-//                 if(matrix[i][j] == -1){
-//                     matrix[i][j] = 0;
-//                 }
-//             }
-//         }
-//     }
-// };
